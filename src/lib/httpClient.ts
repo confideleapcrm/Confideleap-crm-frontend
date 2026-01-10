bedcfd43f638c0ff43286c4096dcc30a5e52b670
confideleapcrm <confideleapcrm@gmail.com>
2026-01-10 17:48:04 +0530

    Update httpClient.ts

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://devapi.confideleap.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://devapi.confideleap.com",
   withCredentials: true,
 });
 
